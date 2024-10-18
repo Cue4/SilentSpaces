2f189af54ff5f8c65f79ede2305bded834f4325d
Caliph Ferguson <caliphferguson@gmail.com>
2024-10-17 21:49:33 -0400

    10

--- server/server.js
+++ server/server.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/dist')))
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')))
+}
 
 app.use('/', routes)
 
